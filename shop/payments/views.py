01e78b703a13ba576e6d9db734a6ece97f5f9c8d
aliensen36 <aliensen65@gmail.com>
2024-08-12 21:30:32 +0300

    Успешное проведение платежа.

--- shop/payments/views.py
+++ shop/payments/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Order
+from yookassa import Payment, Configuration
+from decouple import config, Csv
+import uuid
 
-# Create your views here.
+Configuration.account_id = config('YOOKASSA_SHOP_ID')
+Configuration.secret_key = config('YOOKASSA_SECRET_KEY')
+
+
+def create_order(request):
+    if request.method == 'POST':
+        product_name = request.POST['product_name']
+        amount = request.POST['amount']
+
+        order = Order.objects.create(product_name=product_name, amount=amount)
+
+        base_url = config('BASE_URL')
+        return_url = f"{base_url}/payment_success/"
+
+        payment = Payment.create({
+            "amount": {
+                "value": str(amount),
+                "currency": "RUB"
+            },
+            "confirmation": {
+                "type": "redirect",
+                "return_url": return_url
+            },
+            "capture": True,
+            "description": f"Order {order.id} - {product_name}"
+        }, uuid.uuid4())
+
+        order.payment_status = 'created'
+        order.save()
+
+        return redirect(payment.confirmation.confirmation_url)
+
+    return render(request, 'payments/create_order.html')
+
+
+def payment_success(request):
+    return render(request, 'payments/success.html')
--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,13 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-po+@wtao0wnwh)w8reblj-3vi7tnu=14!r22*27_@j%$zl3(4v'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=Csv())
 
 # Application definition
 
@@ -115,3 +115,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 YOOKASSA_SHOP_ID = config('YOOKASSA_SHOP_ID')
 YOOKASSA_SECRET_KEY = config('YOOKASSA_SECRET_KEY')
+
+
